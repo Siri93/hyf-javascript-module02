bedc7459532c963163ca48ea010b49022486901a
Siri93 <36901593+Siri93@users.noreply.github.com>
2018-04-15 01:03:13 +0200

    Create main.js

---
+++ Week06/main.js
@@ -0,0 +1,10 @@
+// this function give a sum results of three numbers
+function getTotal(num1, num2, num3){
+    var total;
+    total = num1 + num2 + num3;
+    return total;
+  }
+  
+  
+  var sumResult = getTotal(1, 3, 4);
+  console.log(sumResult);
